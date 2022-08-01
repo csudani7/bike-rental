9560cceae8f4ba1f6732e712322f31aa205fb9d9
Nayan Kyada <nayan.kayada@gupshup.io>
2022-08-01 19:13:10 +0530

    dropdown

--- src/pages/CreateBike/index.js
+++ src/pages/CreateBike/index.js
@@ -1,97 +1,102 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import DropDown from "../../Components/DropDown";
+import { StarIcon } from "@heroicons/react/solid";
 
+const colors = [
+  { id: 1, name: "Wade Cooper" },
+  { id: 2, name: "Arlene Mccoy" },
+  { id: 3, name: "Devon Webb" },
+  { id: 4, name: "Tom Cook" },
+  { id: 5, name: "Tanya Fox" },
+  { id: 6, name: "Hellen Schmidt" },
+  { id: 7, name: "Caroline Schultz" },
+  { id: 8, name: "Mason Heaney" },
+  { id: 9, name: "Claudie Smitham" },
+  { id: 10, name: "Emil Schaefer" },
+];
+
+const locations = [
+  { id: 1, name: "Wade Cooper" },
+  { id: 2, name: "Arlene Mccoy" },
+  { id: 3, name: "Devon Webb" },
+  { id: 4, name: "Tom Cook" },
+  { id: 5, name: "Tanya Fox" },
+  { id: 6, name: "Hellen Schmidt" },
+  { id: 7, name: "Caroline Schultz" },
+  { id: 8, name: "Mason Heaney" },
+  { id: 9, name: "Claudie Smitham" },
+  { id: 10, name: "Emil Schaefer" },
+];
 function CreateBike() {
   const [model, setModel] = useState();
-  const [color, setColor] = useState();
-  const [location, setLocation] = useState();
+  const [color, setColor] = useState(colors[0]);
+  const [location, setLocation] = useState(locations[0]);
   const [rating, setRating] = useState();
+  useEffect(() => {
+    console.log(color);
+  }, [color]);
   return (
-    <div>
+    <form>
       <div>
-        <label
-          htmlFor="model"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Model
-        </label>
-        <div className="mt-1">
-          <input
-            value={model}
-            onChange={(e) => {
-              setModel(e.target.value);
-            }}
-            type="model"
-            name="model"
-            id="model"
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            placeholder="you@example.com"
-          />
+        <div>
+          <label
+            htmlFor="model"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Model
+          </label>
+          <div className="mt-1">
+            <input
+              value={model}
+              onChange={(e) => {
+                setModel(e.target.value);
+              }}
+              type="model"
+              name="model"
+              id="model"
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <label
-          htmlFor="model"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Color
-        </label>
-        <div className="mt-1">
-          <input
+        <div>
+          <DropDown
+            title={"color"}
+            options={colors}
+            setValue={setColor}
             value={color}
-            onChange={(e) => {
-              setColor(e.target.value);
-            }}
-            type="model"
-            name="model"
-            id="model"
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            placeholder="you@example.com"
-          />
+          ></DropDown>
         </div>
-      </div>
-      <div>
-        <label
-          htmlFor="model"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Location
-        </label>
-        <div className="mt-1">
-          <input
+        <div>
+          <label
+            htmlFor="model"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Location
+          </label>
+          <DropDown
+            title={"location"}
+            options={locations}
+            setValue={setLocation}
             value={location}
-            onChange={(e) => {
-              setLocation(e.target.value);
-            }}
-            type="model"
-            name="model"
-            id="model"
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            placeholder="you@example.com"
-          />
+          ></DropDown>
+        </div>
+        <div>
+          <label
+            htmlFor="model"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Rating
+          </label>
+          <div className="mt-1">
+            <StarIcon className="h-5 w-5" />
+            <StarIcon className="h-5 w-5" />
+            <StarIcon className="h-5 w-5" />
+            <StarIcon className="h-5 w-5" />
+            <StarIcon className="h-5 w-5" />
+          </div>
         </div>
       </div>
-      <div>
-        <label
-          htmlFor="model"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Rating
-        </label>
-        <div className="mt-1">
-          <input
-            value={rating}
-            onChange={(e) => {
-              setRating(e.target.value);
-            }}
-            type="model"
-            name="model"
-            id="model"
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            placeholder="you@example.com"
-          />
-        </div>
-      </div>
-    </div>
+    </form>
   );
 }
 
